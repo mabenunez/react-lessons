955c9757555c8bf2cf034a2a4ff588642dd56f0b
mabenunez <39781996+mabenunez@users.noreply.github.com>
2019-09-12 10:43:13 -0300

    added dinamic route

--- rutas/src/App.js
+++ rutas/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Carrito from './components/carrito/Carrito';
 import Prueba from './components/prueba/Prueba';
+import Dinamica from './components/dinamica/Dinamica'
 import './App.css';
 
 class App extends Component {
@@ -11,7 +12,7 @@ class App extends Component {
         <Router>
           <Route path={"/carrito"} component={Carrito}/>
           <Route path={"/prueba"} component={Prueba}/>
-          
+          <Route path={'/dinamica/:test'} component={Dinamica}/>
         </Router>
         
       </div>
