835a001a54caf52a07bf57dda7e3c4ef49500227
mabenunez <39781996+mabenunez@users.noreply.github.com>
2019-09-20 11:47:25 -0300

    async/await of json placeholder data. Mapping of that array

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      monsters : []
+    }
+  }
+  /* Promises
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters : users}))
+  }*/
+  async componentDidMount() {
+    try {
+      const data = await fetch('https://jsonplaceholder.typicode.com/users')
+      const jsonData = await data.json()
+      this.setState({
+        monsters : jsonData
+      })
+    } catch (err){
+      console.log(err)
+    }
+  }
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.monsters.map((monster, monsterId) => {
+            return (
+              <li key={monsterId}>{monster.name}</li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
-
 export default App;
